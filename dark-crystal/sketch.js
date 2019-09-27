5dcfda74d64913354396ed4ec3e41180cf948295
evgenyseleznev <evgenii.seleznev@postmates.com>
2019-09-27 15:25:49 -0700

    code update

--- dark-crystal/sketch.js
+++ dark-crystal/sketch.js
@@ -1,15 +1,21 @@
 let startA = 0;
 
 function setup() {
-  createCanvas(600, 400);
+  createCanvas(windowWidth, windowHeight);
   angleMode(DEGREES);
+}
+
+// On window resize, update the canvas size
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
   background(0, 20);
+  // Move coordinates to the center for this object
+  push();
   translate(width / 2, height / 2);
-  stroke(0);
-
+  noStroke();
   let w = 100;
   let h = 100;
   let angle = startA;
@@ -21,6 +27,8 @@ function draw() {
     curveVertex(x, y);
     angle += 10;
   }
+  // Restore coordinates to default
+  pop();
 
   endShape(CLOSE);
   fill(200, 20);
